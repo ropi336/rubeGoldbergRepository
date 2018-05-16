572276a0ebf91ebbc63b393d1d86068eac463f29
Gregory Menyhart <s5064488@bournemouth.ac.uk>
2018-05-16 19:53:40 +0100

    Added Start button

--- Assets/ballBehaviour.cs
+++ Assets/ballBehaviour.cs
@@ -6,7 +6,7 @@ public class ballBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //gameObject.SetActive(false);
+        gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
